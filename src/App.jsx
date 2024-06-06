87536e8ae88aea1274d284c6d4ba1ecd7ba390e9
Rudresh Soiroji <rudreshsoiroji459@gmail.com>
2024-06-07 02:48:53 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import image from "./assets/images/pexels-goumbik-917460.jpg"
+
+function App() {
+
+  return (
+   <div className='w-full h-screen flex flex-wrap justify-center items-center bg-cover bg-no-repeat'
+   style={{backgroundImage: `url(https://images.pexels.com/photos/917460/pexels-photo-917460.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1)`}}
+   >
+    <h1 className='bg-red-200' >test</h1>
+
+   </div>
+  )
+}
+
+export default App
